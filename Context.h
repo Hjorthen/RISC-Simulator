56ca1d90b8afcabc9c49506eee1a028ce8cd20fc
Leth <mleth@deloitte.dk>
2017-11-13 15:15:40 +0100

    Context header

--- Context.h
+++ Context.h
@@ -1,11 +1,8 @@
-#ifndef CONTEXT_H
-#define CONTEXT_H
+#pragma once
 
 int PC = 0;
-int * registers;
-registers = int[32];
+int registers[32];
 for (int i = 0; i < 32; i++) {
 	registers[i] = 0;
 }
 
-#endif
