f868255a69cff819d08b56dddb3e6df303db9563
Hjorthen <Hjorthensebastian@gmail.com>
2017-11-13 16:38:50 +0100

    Fixed no size void error

--- src/Memory.h
+++ src/Memory.h
@@ -1,28 +1,28 @@
 #pragma once
-
 #include <stdlib.h>
 #include <assert.h>
+#include "Types.h"
 
 
 class Memory {
-	void* data;
+	uint8_t* data;
 	size_t s;
 public:
 	Memory(size_t size) {
-		data = malloc(size);
+		data = static_cast<uint8_t*>(malloc(size));
 		s = size;
 	}
 
 	template<typename T>
 	T Load(size_t position) {
 		assert(position + sizeof(T) < s);
-		return *static_cast<T*>(data + position);
+		return *reinterpret_cast<T*>(data + position);
 	}
 
 	template<typename T>
 	void Store(size_t position, T value) {
 		assert(position + sizeof(T) < s);
-		*static_cast<T*>(data + position) = value;
+		*reinterpret_cast<T*>(data + position) = value;
 	}
 
 	static Memory mem;
