ad923588c6e221d00bfe0bb847f99770d4c10adc
Hjorthen <Hjorthensebastian@gmail.com>
2017-11-24 13:09:10 +0100

    Added tests

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,51 @@
 #include "Types.h"
 #include <fstream>
 #include <iostream>
-
+#include <cstring>
 
 
 int main(int argc, char** argv) {
+	if(argc < 2){
+		std::cout << "No file specified to load";
+		return -1;	
+	}
 	std::ifstream dataStream;
 	dataStream.open(argv[1], std::ios::binary);
 	if (dataStream.is_open()) {
 		Instruction ins;
 		Simulator simulator;
 		while (!dataStream.eof() && simulator.Running()) {
-			dataStream >> ins;
+			dataStream.read(reinterpret_cast<char*>(&ins), sizeof(ins));
 			simulator.execute(ins);
 				for (Simulator::RegisterIterator itr = simulator.RegisterBegin();itr != simulator.RegisterEnd();++itr) {
-					std::cout << *itr + '\t';
+					std::cout << *itr << '\t';
 				}
-		
-		}	
-		std::ofstream dump;
-		dump.open("result.bin", std::ios::binary);
-		for (Simulator::RegisterIterator itr = simulator.RegisterBegin();itr != simulator.RegisterEnd();++itr) {
-			dump << *itr;
+				std::cout << '\n';	
 		}
-
+		if(argc > 2){
+			std::ifstream expected;
+			expected.open(argv[2], std::ios::binary | std::ios::ate);
+			if(!expected.is_open()){
+				std::cout << "Failed to open result file";
+				return -1;
+			}
+			std::streamsize fileSize = expected.tellg();
+			Register * expectedResult = new Register[fileSize];
+			expected.read(reinterpret_cast<char*>(&expectedResult), fileSize);
+			if(memcmp(simulator.RegisterBegin(), expectedResult, fileSize) == 0){
+				return 0;
+			}
+			else{
+				return -1;
+			}
+		}
+		else{	
+			std::ofstream dump;
+			dump.open("result.bin", std::ios::binary);
+			for (Simulator::RegisterIterator itr = simulator.RegisterBegin();itr != simulator.RegisterEnd();++itr) {
+				dump << *itr;
+			}
+			return 0;
+		}
 	}
 }
