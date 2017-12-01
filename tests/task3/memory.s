f5abaa69542ba9e525a4d8cedcc62e64f79b15ec
Hjorthen <Hjorthensebastian@gmail.com>
2017-12-01 08:51:28 +0100

    added memory test

---
+++ tests/task3/memory.s
@@ -0,0 +1,17 @@
+LI a0,32
+SW a0, -4(sp)
+LI a0, 0
+LW a0, -4(sp)
+LI a1, 64
+SH a1, -2(sp)
+LI a1, 0
+LH a1, -2(sp)
+LI a2, 128
+SB a2, -1(sp)
+LI a2, 0
+LB a2, -1(sp)
+LI a2, 128
+SB a2, -1(sp)
+LI a2, 0
+LBU a2, -1(sp)
+ecall
